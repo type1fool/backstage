c4e4873c772459105bed4a7d33bd37fade7b5bb4
Owen Bickford <owen@owencode.com>
2024-09-08 17:46:32 -0500

    bump patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Backstage.MixProject do
   def project do
     [
       app: :backstage,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
